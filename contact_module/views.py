13bc56ce881ad44cfae890dca650808de262c17e
TheKidKy <imsajadky@gmail.com>
2022-08-17 23:47:24 -0700

    contact-us update

--- contact_module/views.py
+++ contact_module/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic.edit import FormView
+from django.views.generic import View
 from .forms import contact_model_form
+from django.http import JsonResponse, HttpRequest
 
 # Create your views here.
 
@@ -8,7 +10,21 @@ class ContactUsView(FormView):
     template_name = 'contact.html'
     form_class = contact_model_form
     success_url = '/'
+    JsonResponse ({'success': 'Your message has sent successfully'})
 
     def form_valid(self, form):
         form.save()
         return super().form_valid(form)
+
+
+# class ContactUsView(View):
+#     def get(self, request: HttpRequest):
+#         contact_form = contact_model_form()
+#         context = {'contact_form': contact_form}
+#         return render(request, 'contact.html', context)
+
+#     def post(self, request: HttpRequest):
+#         contact_form = contact_model_form(request.POST)
+#         if contact_form.is_valid():
+#             context = {'contact_form': contact_form}
+#         return render(request, 'contact.html', context)
